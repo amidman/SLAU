ad4d601221517b06ccdb152ea936ad2c2fc012e7
amidman <amidman04@gmail.com>
2023-03-04 01:32:15 +0300

    fix CSR matrix and tests

--- src/CSR.cpp
+++ src/CSR.cpp
@@ -6,10 +6,17 @@ class CSR {
         int row_num;
         int col_num;
         std::vector<T> values;
-        std::vector<T> cols;
-        std::vector<T> rows;
+        std::vector<int> cols;
+        std::vector<int> rows;
     public:
-        T operator[](int i, int j) const {
+        CSR(std::vector<T> &values_i,std::vector<int> &cols_i,std::vector<int> &rows_i){
+            values = values_i;
+            cols = cols_i;
+            rows = rows_i;
+            row_num = rows.size() - 1;
+        }
+
+        T operator()(int i, int j) const {
             for (int k = rows[i]; k < rows[i+1]; ++k) {
                 if (cols[k] == j) {
                     return values[k];
@@ -18,12 +25,13 @@ class CSR {
             return 0;
         }
 
-        T operator*(std::vector<T> vec) const {
+        std::vector<T> operator*(std::vector<T> vec) const {
             std::vector<T> res(row_num);
             for (int i = 0; i < row_num; ++i) {
                 for (int j = rows[i]; j < rows[i+1]; ++j) {
-                    res[i] += values[j] * vec[j];
+                    res[i] += values[j] * vec[cols[j]];
                 }
             }
+            return res;
         }
 };
--- test/test_matrix.cpp
+++ test/test_matrix.cpp
@@ -1,7 +1,50 @@
 #include"../src/tri_diag_matrix.h"
+#include"../src/CSR.cpp"
 #include<iostream>
 #include <gtest/gtest.h>
 
-TEST(matrix, test){
-    ASSERT_EQ(2,2);
+TEST(matrix, values){
+    std::vector<double> values{1, 2, 4, 2, 6};
+    std::vector<int> cols{0, 1, 1, 1, 2};
+    std::vector<int> rows{0, 2, 3, 5};
+    CSR<double> matrix = CSR<double>(values,cols, rows);
+
+    ASSERT_EQ(matrix(0,0),1);
+    ASSERT_EQ(matrix(1,0),0);
+    ASSERT_EQ(matrix(3,0),0);
+    ASSERT_EQ(matrix(-1,0),0);
+}
+
+TEST(matrix, out_of_range){
+    std::vector<double> values{1, 2, 4, 2, 6};
+    std::vector<int> cols{0, 1, 1, 1, 2};
+    std::vector<int> rows{0, 2, 3, 5};
+    CSR<double> matrix = CSR<double>(values,cols, rows);
+
+    ASSERT_EQ(matrix(3,0),0);
+}
+
+TEST(matrix, negative_indexes){
+    std::vector<double> values{1, 2, 4, 2, 6};
+    std::vector<int> cols{0, 1, 1, 1, 2};
+    std::vector<int> rows{0, 2, 3, 5};
+    CSR<double> matrix = CSR<double>(values,cols, rows);
+
+    ASSERT_EQ(matrix(-1,0),0);
+}
+
+TEST(matrix, mult_by_vect){
+    std::vector<double> values{1, 2, 4, 2, 6};
+    std::vector<int> cols{0, 1, 1, 1, 2};
+    std::vector<int> rows{0, 2, 3, 5};
+
+    std::vector<double> b{1, 2, 3};
+
+    CSR<double> matrix = CSR<double>(values,cols, rows);
+
+    std::vector<double> x = matrix*b;
+
+    ASSERT_EQ(x[0],5);
+    ASSERT_EQ(x[1],8);
+    ASSERT_EQ(x[2],22);
 }
